eec8c60dec9ecd3686e60bf36a2fa67e855f1a1f
Kartikay786 <rohit2141451519@emil>
2025-07-04 19:45:20 +0530

    first commit

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ app.use(cors());
 app.use(express.json())
 app.use('/youtubeb',urlRouter)
 
+app.get('/',(req,res)=>{
+    res.send('Server is running.......')
+})
+
 const Port = process.env.PORT || 9000;
 app.listen(Port,()=>{ console.log(`Server is listening on http://localhost:${Port}`)})
